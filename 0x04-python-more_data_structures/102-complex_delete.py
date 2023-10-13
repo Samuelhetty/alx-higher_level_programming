055bf46ebb01131a6de49e61e5b576b2028473cf
Henrietta Onoge <hetty8004@gmail.com>
2023-10-12 22:07:21 -0700

    task 10 done

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new = {ky: val for ky, val in a_dictionary.items() if val == value}
-    for key in new:
-        a_dictionary.pop(key)
-        return a_dictionary
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
