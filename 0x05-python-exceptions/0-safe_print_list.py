360f856b2d31983cff70b922a73cf333dcd211c3
Henrietta Onoge <hetty8004@gmail.com>
2023-10-23 14:12:19 -0700

    completed tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num_elements = 0
+    nb_print = 0
     for aa in range(x):
         try:
             print("{}".format(my_list[aa]), end='')
-            num_element += 1
+            nb_print += 1
         except IndexError:
             break
     print()
-    return num_element
+    return nb_print
