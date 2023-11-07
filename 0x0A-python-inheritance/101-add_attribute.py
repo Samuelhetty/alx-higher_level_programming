a65c1d3be6ea562ebdeaae80d97ba940ec80b8d3
Henrietta Onoge <hetty8004@gmail.com>
2023-11-06 18:03:23 -0800

    completed tasks

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to adds a new attribute to an object if it’s possible"""
+
+def add_attribute(obj, attr_name, attr_value):
+    if hasattr(obj, attr_name):
+        raise TypeError("Can't add a new attribute")
+    else:
+        setattr(obj, attr_name, attr_value)
