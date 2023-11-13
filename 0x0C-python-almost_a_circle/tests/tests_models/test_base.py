aba0c83232616055ce9c932e8dc5c089df7c01d4
Henrietta Onoge <hetty8004@gmail.com>
2023-11-13 09:18:15 -0800

    test models

--- 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Module for Base unit tests.'''
+"""Module for Base unit tests."""
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -7,87 +7,87 @@ from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    '''Tests the Base class.'''
+    """Tests the Base class."""
 
     def setUp(self):
-        '''Imports module, instantiates class'''
+        """Imports module, instantiates class"""
         Base._Base__nb_objects = 0
         pass
 
     def tearDown(self):
-        '''Cleans up after each test_method.'''
+        """Cleans up after each test_method."""
         pass
 
-    def test_A_nb_objects_private(self):
-        '''Tests if nb_objects is private class attribute.'''
+    def test_nb_objects_private(self):
+        """Tests if nb_objects is private class attribute."""
         self.assertTrue(hasattr(Base, "_Base__nb_objects"))
 
-    def test_B_nb_objects_initialized(self):
-        '''Tests if nb_objects initializes to zero.'''
+    def test_nb_objects_initialized(self):
+        """Tests if nb_objects initializes to zero."""
         self.assertEqual(getattr(Base, "_Base__nb_objects"), 0)
 
-    def test_C_instantiation(self):
-        '''Tests Base() instantiation.'''
+    def test_instantiation(self):
+        """Tests Base() instantiation."""
         b = Base()
         self.assertEqual(str(type(b)), "<class 'models.base.Base'>")
         self.assertEqual(b.__dict__, {"id": 1})
         self.assertEqual(b.id, 1)
 
-    def test_D_constructor(self):
-        '''Tests constructor signature.'''
+    def test_constructor(self):
+        """Tests constructor signature."""
         with self.assertRaises(TypeError) as e:
             Base.__init__()
         msg = "__init__() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), msg)
 
-    def test_D_constructor_args_2(self):
-        '''Tests constructor signature with 2 notself args.'''
+    def test_constructor_args_2(self):
+        """Tests constructor signature with 2 notself args."""
         with self.assertRaises(TypeError) as e:
             Base.__init__(self, 1, 2)
         msg = "__init__() takes from 1 to 2 positional arguments but 3 \
 were given"
         self.assertEqual(str(e.exception), msg)
 
-    def test_E_consecutive_ids(self):
-        '''Tests consecutive ids.'''
+    def test_consecutive_ids(self):
+        """Tests consecutive ids"""
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id + 1, b2.id)
 
-    def test_F_id_synced(self):
-        '''Tests sync between class and instance id.'''
+    def test_id_synced(self):
+        """Tests sync between class and instance id"""
         b = Base()
         self.assertEqual(getattr(Base, "_Base__nb_objects"), b.id)
 
-    def test_F_id_synced_more(self):
-        '''Tests sync between class and instance id.'''
+    def test_id_synced_more(self):
+        """Tests sync between class and instance id"""
         b = Base()
         b = Base("Foo")
         b = Base(98)
         b = Base()
         self.assertEqual(getattr(Base, "_Base__nb_objects"), b.id)
 
-    def test_G_custom_id_int(self):
-        '''Tests custom int id.'''
+    def test_custom_id_int(self):
+        """Tests custom int id"""
         i = 98
         b = Base(i)
         self.assertEqual(b.id, i)
 
-    def test_G_custom_id_str(self):
-        '''Tests custom int id.'''
+    def test_custom_id_str(self):
+        """Tests custom int id"""
         i = "FooBar"
         b = Base(i)
         self.assertEqual(b.id, i)
 
-    def test_G_id_keyword(self):
-        '''Tests id passed as keyword arg.'''
+    def test_id_keyword(self):
+        """Tests id passed as keyword arg"""
         i = 421
         b = Base(id=i)
         self.assertEqual(b.id, i)
 
-    # ----------------- Tests for #15 ------------------------
-    def test_H_to_json_string(self):
-        '''Tests to_json_string() signature:'''
+    # ----------------- Tests #15 ------------------------
+    def test_to_json_string(self):
+        """Tests to_json_string() signature"""
         with self.assertRaises(TypeError) as e:
             Base.to_json_string()
         s = "to_json_string() missing 1 required positional argument: \
@@ -157,9 +157,9 @@ were given"
         dictionary = dictionary.replace("'", '"')
         self.assertEqual(dictionary, json_dictionary)
 
-    # ----------------- Tests for #17 ------------------------
-    def test_H_test_from_json_string(self):
-        '''Tests to_json_string() signature:'''
+    # ----------------- Tests #17 ------------------------
+    def test_Test_from_json_string(self):
+    """Tests to_json_string() signature:"""
         with self.assertRaises(TypeError) as e:
             Base.from_json_string()
         s = "from_json_string() missing 1 required positional argument: \
@@ -209,9 +209,9 @@ were given"
             Rectangle.to_json_string(list_in))
         self.assertEqual(list_in, list_out)
 
-        # ----------------- Tests for #16 ------------------------
-    def test_I_save_to_file(self):
-        '''Tests save_to_file() method.'''
+        # ----------------- Tests #16 ------------------------
+    def test_save_to_file(self):
+        """Tests save_to_file() method"""
         import os
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
@@ -254,9 +254,9 @@ were given"
         with open("Square.json", "r") as file:
             self.assertEqual(len(file.read()), 38)
 
-        # ----------------- Tests for #18 ------------------------
-    def test_J_create(self):
-        '''Tests create() method.'''
+        # ----------------- Tests #18 ------------------------
+    def test_create(self):
+        """Tests create() method."""
         r1 = Rectangle(3, 5, 1)
         r1_dictionary = r1.to_dictionary()
         r2 = Rectangle.create(**r1_dictionary)
@@ -264,9 +264,9 @@ were given"
         self.assertFalse(r1 is r2)
         self.assertFalse(r1 == r2)
 
-        # ----------------- Tests for #19 ------------------------
-    def test_K_load_from_file(self):
-        '''Tests load_from_file() method.'''
+        # ----------------- Tests #19 ------------------------
+    def test_load_from_file(self):
+        """Tests load_from_file() method."""
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         list_in = [r1, r2]
