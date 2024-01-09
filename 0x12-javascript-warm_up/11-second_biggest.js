dc5d102389357618e3f3cd1b7af42eec317adff6
Henrietta Onoge <hetty8004@gmail.com>
2024-01-09 02:59:22 -0800

    complete task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const argv = process.argv.slice(2);
-  console.log(argv.sort()[argv.length - 2]);
+  const cont = process.argv.sort();
+  console.log(cont.reverse()[1]);
 }
